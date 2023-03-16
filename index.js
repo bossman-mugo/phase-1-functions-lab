4704362df69fb55b2684d2400e5679284503d400
Eric <mugoericpg20@gmail.com>
2023-03-16 15:49:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+    const hq = 42;
+    const distanceInBlocks =  Math.abs(hq - pickupLocation);
+    return distanceInBlocks;
+
+}
+
+
+function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    const distanceInFeet = blocks * 264;
+  return distanceInFeet;
+  }
+
+ function distanceTravelledInFeet(startBlock, endBlock) {
+        const distanceInBlocks = Math.abs(endBlock - startBlock);
+        const distanceInFeet = distanceInBlocks * 264;
+        return distanceInFeet;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+
+    if (distanceInFeet <= 400) {
+      return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+
+
+
+
